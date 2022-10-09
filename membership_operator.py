6a6cb20be6ff8372080c7e181f6c734e9eb30995
bavabala <62609269+bavabala@users.noreply.github.com>
2022-10-09 07:35:48 +0530

    Add files via upload
    operator

---
+++ membership_operator.py
@@ -0,0 +1,10 @@
+#in - returns True,if a sequence with the specified value is present in the object
+#not in -  returns True,if a sequence with the specified value is not present in the object
+
+lst = [4,10,9,82,34]
+
+in_demo = 10 in lst
+print(in_demo)
+
+not_in_demo = 11 not in lst
+print(in_demo)
