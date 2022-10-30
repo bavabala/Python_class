c3e394c94c73bda9a094e2ce3fe645509487ac17
bavabala <62609269+bavabala@users.noreply.github.com>
2022-10-30 18:11:42 +0530

    Add files via upload

--- types_of_variables.py
+++ types_of_variables.py
@@ -1,27 +1,32 @@
-#types of variables ==>local variable,global variable
-#local variables
-"""
-local variables are the variables which are define inside the class
-its scope within the function itself
-you cannot access outside the function
-even parameters are local variable
-"""
 #global variable
-"""
-are the variables which are define outside the function
-Its scope throughout the python class
-"""
+#local variable
+#intance variable
 
+#static/class variable:
+#-this variable is declared inside the class and outside the method/constructor
+#scope throughout the class
+#class variable is accessed by the class name
+#syntax:<class_name>.<class_variable_name>
+#it is used for memory management
+#shares the same memory
+#class variable can be called class name/object name
 
-rno=90              #global variable
-def info():
-    name = "Bava"   #local variable
-    global address
-    address = "chennai"
-    print(f"Hi{rno},{name},{address}")
-    return name
-data = info() #object creation
-print(data)
-print("Rno: ",rno)
-print("Address: ",address)
+class school:
+    school_name = "Coder" #class/static variable
 
+    def __init__(self,name,rno):
+        self.name=name
+        self.rno = rno
+        
+    def display(self):
+        print(f"Hi{self.name} from{school.school_name},your rno is{self.rno}!")
+
+print(school.school_name)
+obj = school("bava",25)
+obj.display()
+print(obj.school_name)
+
+obj1 = school("dhivya",21)
+obj1.display()
+print(obj1.school_name)
+print(obj1.name)
