2cf13253f192f19f759448e6474f4742f3533761
bavabala <62609269+bavabala@users.noreply.github.com>
2022-11-20 18:50:45 +0530

    Add files via upload

---
+++ Inheritance/multilevel_inheritance.py
@@ -0,0 +1,23 @@
+class grandpa:
+    def field(self):
+        print("field")
+    
+class dad(grandpa):
+    def flat(self):
+        print("flat")
+
+    def bike(self):
+        print("bike")
+class son(dad):
+    def mobile(self):
+        print("mobile")
+
+obj = son()
+obj.mobile()
+obj.field()
+obj.bike()
+obj.flat()
+
+
+
+    
